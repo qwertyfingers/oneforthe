ce7e516b5a6dac43f0c0b1ad32a304ab82ba05f5
Barry Park <barry.park1@hotmail.com>
2013-01-14 13:17:59 +0000

    added admin to urls.py

--- oneforthe/urls.py
+++ oneforthe/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+from conf.urls import include
+admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^$', 'money.views.display_image'), #Look for the root webpage
-)
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
